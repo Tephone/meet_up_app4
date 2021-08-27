6d7e9891b662e9953d62f9bcea7233b12ce6b49d
naoko kasahara <kassa0918@gmail.com>
2021-08-28 01:34:19 +0900

    講師作成削除機能、生徒にカラム追加、その他view作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :admins, controllers: {sessions: 'admins/sessions'}
+  devise_for :students, controllers: {sessions: 'students/sessions', registrations: 'students/registrations'}
   devise_for :teachers
-  devise_for :students
-  devise_for :admins
+  root 'tops#show'
   resource :top, only: [:show]
+  namespace :admins do
+    resources :teachers, only: %i[index new create destroy]
+  end
+  resource :admin, only: [:show]
+  resource :student, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
