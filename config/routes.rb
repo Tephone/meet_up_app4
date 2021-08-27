862da0b39c5266df4a78bdc6dc36a6bad3173cce
naoko kasahara <kassa0918@gmail.com>
2021-08-27 21:50:45 +0900

    devise関連model, view生成, 日本語化, 時刻日本語化

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :teachers
+  devise_for :students
+  devise_for :admins
+  resource :top, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
