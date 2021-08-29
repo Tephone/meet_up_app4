4840df05eead41c9dd8c838a962dbd7e7dac6c90
naoko kasahara <kassa0918@gmail.com>
2021-08-30 03:17:29 +0900

    sgcop先生

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,5 @@ class Review < ApplicationRecord
   belongs_to :teacher
   belongs_to :lesson
   validates :content, presence: true
-  validates :teacher_id, uniqueness: {scope: :lesson_id}
+  validates :teacher_id, uniqueness: { scope: :lesson_id }
 end
