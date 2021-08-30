4240eee399c14739a107f2f3c765140560824011
naoko kasahara <kassa0918@gmail.com>
2021-08-31 00:52:19 +0900

    定期チケット購入メゾット定義、rake＿task作成

--- app/models/choice_monthly_plan.rb
+++ app/models/choice_monthly_plan.rb
@@ -3,4 +3,10 @@ class ChoiceMonthlyPlan < ApplicationRecord
   belongs_to :monthly_plan
   validates :monthly_plan, presence: true
   validates :student_id, uniqueness: { scope: :monthly_plan_id }
+
+  def self.ticket_subscription
+    self.all.each do |choice_monthly_plan|
+      choice_monthly_plan.student.purchase_tickets.create!(ticket_id: choice_monthly_plan.monthly_plan.ticket.id, deadline: Date.current.end_of_month)
+    end
+  end
 end
