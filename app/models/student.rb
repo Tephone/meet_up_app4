215bb31b65593c05202a3180fa1daaa4b03c5b60
naoko kasahara <kassa0918@gmail.com>
2021-09-06 00:35:50 +0900

    課題3-2修正

--- app/models/student.rb
+++ app/models/student.rb
@@ -9,6 +9,11 @@ class Student < ApplicationRecord
   has_many :lessons, through: :lesson_reservations, source: :lesson
   has_one :choice_monthly_plan, dependent: :destroy
 
+  after_create do
+    ticket = Ticket.find_by(lesson_count: 1)
+    Student.last.purchase_tickets.create!(ticket_id: ticket.id)
+  end
+
   def remaining_lesson_count
     valid_ticket_ids = self.purchase_tickets.valid.select(:ticket_id)
     lesson_count = self.tickets.where(id: valid_ticket_ids).sum(:lesson_count)
