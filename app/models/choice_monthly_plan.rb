d4efdfb74900612baa555db05bbe8ee316be844b
naoko kasahara <kassa0918@gmail.com>
2021-09-06 00:57:43 +0900

    課題3-3, 3-4修正

--- app/models/choice_monthly_plan.rb
+++ app/models/choice_monthly_plan.rb
@@ -1,12 +1,15 @@
 class ChoiceMonthlyPlan < ApplicationRecord
   belongs_to :student
   belongs_to :monthly_plan
-  validates :monthly_plan, presence: true
   validates :student_id, uniqueness: { scope: :monthly_plan_id }
-
-  def self.ticket_subscription
-    self.find_each do |choice_monthly_plan|
-      choice_monthly_plan.student.purchase_tickets.create!(ticket_id: choice_monthly_plan.monthly_plan.ticket.id, deadline: Date.current.end_of_month)
+  
+  class << self
+    def ticket_subscription
+      ApplicationRecord.transaction do
+        self.find_each do |choice_monthly_plan|
+          choice_monthly_plan.student.purchase_tickets.create!(ticket_id: choice_monthly_plan.monthly_plan.ticket.id, deadline: Date.current.end_of_month)
+        end
+      end
     end
   end
 end
